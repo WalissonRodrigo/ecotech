72dfc4de91035a740133be1d993a83fabb26a23f
Walisson Rodrigo <walissonrodrigo@outlook.com>
2018-05-20 10:13:08 -0300

    force redirect to http

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -2,17 +2,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\URL;
 
 class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
         /*
-            if (!$request->secure() && env('APP_ENV') === 'prod') {
+            if(!$request->secure() && env('APP_ENV') === 'prod') {
                 return redirect()->secure($request->getRequestUri());
             }
-            */
-            URL::forceSchema('http');
-            return $next($request); 
+        
+            //URL::forceSchema('http');
+        */ 
+        return $next($request);
+
     }
 }
